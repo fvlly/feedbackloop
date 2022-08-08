ede9228cc960ad2c5aa201264cb23d930c659642
Fvlly <luki0000001@gmail.com>
2022-08-09 00:16:28 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send({
+        'name': "nonymous dev"
+    })
+})
+
+
+const PORT = process.env.PORT|| 5000
+
+app.listen(PORT,()=>{
+    console.log('App is up and running on PORT:',PORT);
+})
