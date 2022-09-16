e1bca32cecd2708126adb96aee0ffd2a4bc68e1b
Fvlly <luki0000001@gmail.com>
2022-09-16 01:12:48 +0100

    resolving user credit update v3(without process.env.NODE_ENV ==='production')

--- client/src/components/Billing.jsx
+++ client/src/components/Billing.jsx
@@ -10,8 +10,9 @@ const Billing = (props) => {
     description="$5 for 5 email credits"
     amount={500}
     token={token=>props.handleToken(token)}
-    stripeKey={process.env.NODE_ENV ==='production'? process.env.STRIPE_PUBLISHABLE_KEY : import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY }
+    stripeKey={ import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY }
     >
+      {/* process.env.NODE_ENV === "production" */}
       <Button
       bgColor={'gray.300'}
       color='#ff6d60'
